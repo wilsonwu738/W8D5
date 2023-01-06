93c9c164cd4e42aef9838f4f58b547afee2f9105
Wilson Wu <wuyecheng738@gmail.com>
2023-01-06 16:24:56 -0500

    mybind

--- argument_exercises/sum.js
+++ argument_exercises/sum.js
@@ -1,10 +1,22 @@
 function sum(num1, ...othernums) {
     let total = num1
-    othernums.forEach((ele) => {
-        total += ele;
-    });
-    return total
+    othernums.forEach(function (ele) {
+      total += ele;
+    })
+    return total;
 }
 
-console.log(sum(1, 2, 3, 4))
+console.log(sum(1, 2, 3, 4));
 console.log(sum(1, 2, 3, 4, 5))
+
+
+function sum(nums) {
+  let total = 0
+  let args = Array.prototype.slice.call(arguments); //make a copy of arguments and make that as an array
+  args.forEach(function (ele) {
+    total += ele;
+  })
+  return total;
+}
+
+console.log(sum(10, 2, 15, 4));
