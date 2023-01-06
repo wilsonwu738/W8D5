4c8edb60bd0f9233e7922663350122598a3d1f6b
Yen Lee <yenny0104@gmail.com>
2023-01-06 14:51:56 -0500

    bind

--- argument_exercises/sum.js
+++ argument_exercises/sum.js
@@ -1,1 +1,10 @@
-function sum(num1,)
+function sum(num1, ...othernums) {
+    let total = num1
+    othernums.forEach((ele) => {
+        total += ele;
+    });
+    return total
+}
+
+console.log(sum(1, 2, 3, 4))
+console.log(sum(1, 2, 3, 4, 5))
